fcbc68a73906baff843c192f2e288e0fd2f79767
mmorgan1247 <mack.morgan@gmail.com>
2023-02-13 18:06:42 -0500

    before dinner

--- main.js
+++ main.js
@@ -2,19 +2,26 @@ function pointClicked(pointID) {
     console.log("point was clicked.");
 
     
-    let rightCol = document.getElementById("right-column");
+    let rightCol = document.getElementById("last-point-clicked");
     rightCol.innerHTML = "Last point clicked: " + pointID;
 
-    let clickedDiv = document.getElementById(pointID);
-    clickedDiv.style.color = "blueviolet";
 
+    let clickedDiv = document.getElementById(pointID);
+    point.classList.toggle("clicked"); //This does not work.
+
+}
+
+function pointHovered(point) {
+    point.classList.toggle("hovered");
 }
 
 function newPointSubmission() {
     let xVal = document.getElementById("select-x-coord").value;
     let yVal = document.getElementById("select-y-coord").value;
 
+//unfinished.
     if(!x.equals("X-Coordinate") && !yVal.equals("Y-Coordinate")) {
-        
+        let canvas = document.getElementById('')
+        const ctx = canvas.getContext('2d');
     }
 }
